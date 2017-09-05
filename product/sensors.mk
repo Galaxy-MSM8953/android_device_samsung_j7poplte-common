c5b9e9cc0fb0909bab8b7135ebfb0911869fc7b2
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:46:25 +0200

    j7poplte-common: sensors: Adjust for our usecase
    * (minz1): Bring in hals.conf from the common tree
    * Rename hals.conf -> _hals.conf so legitimate
      MultiHAL won't attempt to load our HALs.
    * Rename multihal -> multihal-j7poplte so soong
      doesn't complain about duplicate package names.
    * Remove USE_SENSOR_MULTI_HAL flag requirement so
      we can actually build it.
    * Use TARGET_BOARD_PLATFORM variable in package
      name because it's a common tree.
    
    Change-Id: Id75ff7a533d13c125fea401bb43f9b586d6875cd

--- product/sensors.mk
+++ product/sensors.mk
@@ -3,3 +3,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+
+
+# Config
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
