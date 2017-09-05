7bc82a40dc25d9caf7bf758ba5ac3b8d0c628c80
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    j7poplte-common: Build our MultiHAL sensors HAL
    Change-Id: Icce5b5f487321e79c7657ac66eefd571065ee010

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,9 +1,12 @@
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8953
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
 
 # Config
 PRODUCT_COPY_FILES += \
