416b70c4d39db6fc1ea897f2abdc609dcf5c270f
minz1 <emerytang@gmail.com>
2017-11-01 15:39:49 -0500

    j7poplte-common: CM -> Lineage

--- lineage.mk
+++ lineage.mk
@@ -5,8 +5,8 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy J7 Perx
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
