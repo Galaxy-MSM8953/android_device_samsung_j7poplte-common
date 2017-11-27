02cfe044115fc1add5897a786a26742f6e260f3e
minz1 <emerytang@gmail.com>
2017-11-26 22:10:45 -0600

    j7poplte-common: add missing 64-bit include
    * Explains a lot...

--- lineage.mk
+++ lineage.mk
@@ -9,6 +9,7 @@ TARGET_SCREEN_HEIGHT := 1280
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 ## Device identifier. This must come after all inclusions
