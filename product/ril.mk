d1d67f3abc415edd9543601c1e077c6102fac8bb
minz1 <emerytang@gmail.com>
2018-03-09 01:22:31 -0600

    j7poplte-common: commonize shared stuff

--- product/ril.mk
+++ product/ril.mk
@@ -4,6 +4,12 @@ PRODUCT_PACKAGES += \
     librilutils \
     rild
 
+# QMI Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
