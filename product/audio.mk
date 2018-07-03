30cff7337ff2d66340f553251ee578102de634f7
Sean Hoyt <seanhoyt963@gmail.com>
2018-07-03 00:27:17 -0400

    WIP: Use stock audio configs for some parts (1/2)
    Change-Id: Iecf57a82fafd9091c76afccfbb5fd493a144a959

--- product/audio.mk
+++ product/audio.mk
@@ -1,3 +1,5 @@
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
